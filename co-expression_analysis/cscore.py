2111d9a091b7ec85bd9530dd90291180fa2228d8
sdavenport <sdavenport@fry.wi.mit.edu>
2025-07-16 11:58:53 -0400

    module expression per individual matrixes

--- co-expression_analysis/cscore.py
+++ co-expression_analysis/cscore.py
@@ -15,9 +15,11 @@ args = parser.parse_args()
 
 adata = sc.read_h5ad(args.adata)
 
-cell_adata = adata[adata.obs[args.celltype_col]==args.current_celltype, :]
+adata.obs["Celltype_clean"] = adata.obs[args.celltype_col].astype(str).str.replace(r" \+ ", "_", regex=True).str.replace(r" ", "_", regex=True).str.replace(r"\(", "", regex=True).str.replace(r"\)", "", regex=True).str.replace("/", "-")
 
-cell_adata.write("/lab/solexa_sun/shared_Data/scRNAseq/brain/combined_data/AD_microglia_6_25.h5ad")
+cell_adata = adata[adata.obs["Celltype_clean"]==args.current_celltype, :]
+
+print(cell_adata.shape)
 
 if cell_adata.X.max() < 100: #already normalized... resetting to raw counts
     cell_adata.X=cell_adata.layers["counts"]
@@ -26,34 +28,19 @@ if cell_adata.raw is None:
 
 #scale counts by seq depths
 sc.pp.normalize_total(cell_adata, target_sum=1)
-mean_exp = (cell_adata.X).sum(axis=0).A1
-mean_exp_df = pd.DataFrame({'gene': cell_adata.var_names, 'mean_expression': mean_exp})
-top_genes_df = mean_exp_df.sort_values(by='mean_expression', ascending=False).head(5000) #selecting top 5000 genes by expression
-top_genes_indices = top_genes_df.index.astype(int).to_numpy()
-
-if "CON" in cell_adata.obs.Condition.unique():
-    cell_adata_control = cell_adata[cell_adata.obs["Condition"] == 'CON', :]
-elif "Control" in cell_adata.obs.Condition.unique():
-    cell_adata_control = cell_adata[cell_adata.obs["Condition"] == 'Control', :]
-else:
-    print("Unsure control coding in dataset... cannot proceed")
-    sys.exit(1)
-
-cell_adata_disease = cell_adata[cell_adata.obs["Condition"] != 'Control', :]
-
-res_control = CSCORE(cell_adata_control, top_genes_indices)
-res_disease = CSCORE(cell_adata_disease, top_genes_indices)
+nonzero_expr = (cell_adata.X > 0)
+frac_expressed = nonzero_expr.sum(axis=0).A1 / cell_adata.X.shape[0]
+expr_frac_df = pd.DataFrame({'gene': cell_adata.var_names,'frac_expressed': frac_expressed})
+expressed_genes_df = expr_frac_df[expr_frac_df['frac_expressed'] >= 0.10]
+expressed_genes_indices = expressed_genes_df.index.astype(int).to_numpy()
+print("number of genes in over 10pct of cells:", len(expressed_genes_df['gene']))
+res = CSCORE(cell_adata, expressed_genes_indices)
 
 #saving outputs for R...
-np.savetxt(f"{args.output_prefix}_ctrl_coexpr.csv", res_control[0], delimiter=",")
-np.savetxt(f"{args.output_prefix}_ctrl_pvals.csv", res_control[1], delimiter=",")
-np.savetxt(f"{args.output_prefix}_ctrl_teststats.csv", res_control[2], delimiter=",")
-
-np.savetxt(f"{args.output_prefix}_disease_coexpr.csv", res_disease[0], delimiter=",")
-np.savetxt(f"{args.output_prefix}_disease_pvals.csv", res_disease[1], delimiter=",")
-np.savetxt(f"{args.output_prefix}_disease_teststats.csv", res_disease[2], delimiter=",")
+np.savetxt(f"{args.output_prefix}_coexpr.csv", res[0], delimiter=",")
+np.savetxt(f"{args.output_prefix}_pvals.csv", res[1], delimiter=",")
+np.savetxt(f"{args.output_prefix}_teststats.csv", res[2], delimiter=",")
 
 #Save selected gene names (same order as matrices)
-genes_selected = top_genes_df['gene'].to_numpy()
-np.savetxt(f"{args.output_prefix}_ctrl_genes.csv", genes_selected, fmt='%s')
-np.savetxt(f"{args.output_prefix}_disease_genes.csv", genes_selected, fmt='%s')
+genes_selected = expressed_genes_df['gene'].to_numpy()
+np.savetxt(f"{args.output_prefix}_genes.csv", genes_selected, fmt='%s')
---
+++ co-expression_analysis/indv_module_matrix.py
@@ -0,0 +1,49 @@
+import scanpy as sc
+import numpy as np
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--adata", required=True)
+parser.add_argument("--modules", required=True)
+parser.add_argument("--matrix", required=True)
+parser.add_argument("--celltype_col", required=True)
+parser.add_argument("--current_celltype", required=True)
+args = parser.parse_args()
+
+def get_individuallevel_module_exp(adata, module_df):
+    modules = module_df['module'].unique()
+    if 'Individual' in list(adata.obs.columns):
+        individuals = adata.obs['Individual'].unique()
+    elif 'individual_id_anon' in list(adata.obs.columns):
+        individuals = adata.obs['individual_id_anon'].unique()
+    else:
+        individuals = adata.obs['individualID'].unique()
+    matrix = pd.DataFrame(index=modules, columns=individuals, dtype=float)
+    for module_num in list(module_df['module'].unique()):
+        gene_list=list(module_df[module_df['module']==module_num]['gene'])   
+        mod_adata=adata[:, adata.var_names.isin(gene_list)]
+
+        individual_means = {}
+
+        for ind in list(mod_adata.obs.Individual.unique()):
+            ind_adata=mod_adata[mod_adata.obs.Individual==ind]
+            gene_means = np.mean(ind_adata.layers['normalized_counts'], axis=0)
+            module_mean = np.mean(gene_means)
+            individual_means[ind] = module_mean
+        
+        matrix.loc[module_num] = pd.Series(individual_means)
+    
+    return(matrix)
+
+adata = sc.read_h5ad(args.adata)
+
+adata.obs["Celltype_clean"] = adata.obs[args.celltype_col].astype(str).str.replace(r" \+ ", "_", regex=True).str.replace(r" ", "_", regex=True).str.replace(r"\(", "", regex=True).str.replace(r"\)", "", regex=True).str.replace("/", "-")
+
+cell_adata = adata[adata.obs["Celltype_clean"]==args.current_celltype, :]
+
+modules_df=pd.read_csv(args.modules)
+
+matrix=get_individuallevel_module_exp(cell_adata, modules_df)
+
+matrix.to_csv(args.matrix)
