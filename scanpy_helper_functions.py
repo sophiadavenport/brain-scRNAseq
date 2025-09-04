fa71da69f9e5837b0c270b330a4250907067e069
sdavenport <sdavenport@fry.wi.mit.edu>
2025-09-04 15:47:16 -0400

    setting colors for subtypes/celltypes

--- scanpy_helper_functions.py
+++ scanpy_helper_functions.py
@@ -411,3 +411,49 @@ def plot_celltype_bar(adata, x_col, save_string, colorby=None, percent_true=Fals
     ax.set_title(title)
     plt.tight_layout()
     plt.figsave(save_string)
+
+def set_celltype_colors(adata, celltype_class_col=None, celltype_subtype_col=None):
+    '''
+    Used to make colors consistent for subtypes and celltypes across datasets
+    '''
+    broad_colors = {
+        'Excitatory neurons':'#006d2c', #deep green
+        'OPCs + COPs': '#8c510a', #brown
+        'Vascular Cells': '#bebada', #purple
+        'Astrocytes': '#FF7518', #orange
+        'Inhibitory neurons': '#377eb8', #blue
+        'Oligodendrocytes': '#f781bf', #soft pink
+        'Microglia': '#e41a1c' #red
+        }
+    subtype_colors = {
+        #Excitatory neurons (green)
+        'Exc L2-3 CBLN2 LINC02306': "#00441b",'Exc L3-4 RORB CUX2': "#006d2c",'Exc L3-5 RORB PLCH1': "#238b45",'Exc L4-5 RORB GABRG1': "#41ab5d",'Exc L4-5 RORB IL1RAPL2': "#74c476",'Exc L5 ET': "#a1d99b",
+        'Exc L5/6 IT Car3': "#c7e9c0",'Exc L5/6 NP': "#2ca25f",'Exc L5-6 RORB LINC02196': "#66c2a4",'Exc L6 CT': "#99d8c9",'Exc L6 THEMIS NFIA': "#ccece6",'Exc L6b': "#5aae61",'Exc NRGN': "#1b7837",'Exc RELN CHD7': "#d9f0d3",
+        #Inhibitory neurons (blue)
+        'Inh ALCAM TRPM3': "#08306b",'Inh CUX2 MSR1': "#08519c",'Inh ENOX2 SPHKAP': "#2171b5",'Inh FBN2 EPB41L4A': "#4292c6",'Inh GPC5 RIT2': "#6baed6", 'Inh L1 PAX6 CA4': "#9ecae1",
+        'Inh L1-2 PAX6 SCGN': "#c6dbef",'Inh L1-6 LAMP5 CA13': "#9fb3c8",'Inh L3-5 SST MAFB': "#252b6c",'Inh L5-6 PVALB STON2': "#2c7fb8",'Inh L5-6 SST TH': "#3690c0",
+        'Inh L6 SST NPY': "#74a9cf",'Inh LAMP5 NRG1 (Rosehip)': "#a6bddb",'Inh LAMP5 RELN': "#d0d1e6",'Inh PTPRK FAM19A1': "#1d3557",'Inh PVALB CA8 (Chandelier)': "#0570b0",
+        'Inh PVALB HTR4': "#3690c0",'Inh PVALB SULF1': "#74a9cf",'Inh RYR3 TSHZ2': "#a6bddb",'Inh SGCD PDE3A': "#045a8d",'Inh SORCS1 TTN': "#2b8cbe",'Inh VIP ABI3BP': "#627d98",
+        'Inh VIP CLSTN2': "#005082",'Inh VIP THSD7B': "#506680",'Inh VIP TSHZ2': "#41526a",
+        #Astrocytes (orange)
+        'Ast DPP10': "#d94801",'Ast GRM3': "#ff7f2a",'Ast TPST1': "#ffae66",
+        #Microglia (red)
+        'Mic DUSP1': "#660000",'Mic MKI67': "#990000",'Mic P2RY12': "#cc0000",'Mic TPT1': "#e60000",'T cells': "#ff1a1a",
+        #Oligodendrocytes (pink)
+        'Oli OPALIN': "#fa91c9",'Oli RASGRF1': "#ff4da6",
+        #OPCs + COPs (brown)
+        'OPC DOCK5': "#5c4033",'Opc GRIA4': "#8b5a2b",'Opc TPST1': "#d2b48c",
+        #Vascular Cells (purple)
+        'CAMs': "#3f007d",'Fib1': "#54278f",'Fib2': "#6a51a3",'Fib3': "#7b3294",'Per1': "#807dba",'Per2': "#9e9ac8",'aEndo': "#9e4db3",'aSMC': "#b084c1",'capEndo': "#cbc9e2",'vEndo': "#dadaeb",'vSMC': "#decbe4",
+        #NA (black)
+        'NA': "#000000"
+        }
+    if celltype_class_col:
+        colors=[broad_colors[cat] for cat in adata.obs[celltype_class_col].cat.categories]
+        uns_classname=f"{celltype_class_col}_colors"
+        adata.uns[uns_classname]=colors
+    if celltype_subtype_col:
+        colors=[subtype_colors[cat] for cat in adata.obs[celltype_subtype_col].cat.categories]
+        uns_subname=f"{celltype_subtype_col}_colors"
+        adata.uns[uns_subname]=colors
+    return(adata)
