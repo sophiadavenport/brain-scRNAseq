3e5256df06749e4561735ff632d02027f7fdcc1e
sdavenport <sdavenport@wi.mit.edu>
2025-11-12 17:12:47 -0500

    adding trade to evaluate edger output

---
+++ DEG/trade.R
@@ -0,0 +1,29 @@
+library(devtools)
+library(dplyr)
+if (!requireNamespace("TRADEtools", quietly=TRUE)) {
+  message("TRADEtools not found, trying install...")
+  devtools::install_github("immunogenomics/TRADEtools", dependencies=TRUE)
+}
+
+library(TRADEtools)
+
+args <- commandArgs(trailingOnly=TRUE)
+edger_res <- args[1]
+output_rds <- args[2]
+output_sig_genes <- args[3]
+
+results <- read.csv(edger_res)
+
+results <- results |>
+  dplyr::rename(
+    log2FoldChange=logFC,
+    pvalue=PValue
+  )
+results$lfcSE <- 1 / sqrt(results$F + 1e-8)  #crude proxy from F statistic since edgeR doesn't give lfcSE
+
+
+TRADE_output <- TRADE(mode="univariate", results1=results, annot_table=NULL, genes_exclude=NULL, n_sample=NULL)
+sig_genes <- data.frame(TRADE_output$significant_genes_FDR)
+
+saveRDS(TRADE_output, file=output_rds)
+write.csv(sig_genes, output_sig_genes, row.names=FALSE)
